df4d518d2e13894900ac231427c38d0d931f694d
Tobi <dalhof.tobias@gmail.com>
2019-11-15 09:15:19 +0100

    Update fastlane.vue

--- pages/fastlane.vue
+++ pages/fastlane.vue
@@ -5,7 +5,7 @@
         <v-card color="success" dark class="mb-3 elevation-5">
           <v-card-text>
             <div class="mb-1 subheading font-weight-bold">
-              🥳🙌 Special offer sponsored by our friends at <a style="color:#fff" target="_blank" href="https://ecommercefastlane.com/">eCommerceFastlane.com</a>
+              🥳🙌 Special offer sponsored by our friends at <a style="color:#fff" target="_blank" href="https://ecommercefastlane.com/podcast/episode-69/">eCommerceFastlane.com</a>
             </div>
             <div>Try Delm for free with our <strong>extended 30 day trial</strong> instead of just 7 days. Only available from November 15th to November 30th.</div>
           </v-card-text>
